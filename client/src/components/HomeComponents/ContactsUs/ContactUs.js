3f2f953cd1d2623d8ef5d0ad8f6a34020646462a
luskoeqk <77803201+luskoeqk@users.noreply.github.com>
2022-10-19 10:46:48 +0300

    contact us updates

--- client/src/components/HomeComponents/ContactsUs/ContactUs.js
+++ client/src/components/HomeComponents/ContactsUs/ContactUs.js
@@ -2,6 +2,8 @@
 
 import styles from './ContactUs.module.css'
 
+// import addresss from '../../../assets/svg/address.svg'
+
 export const ContactUs = () => {
     return (
         <div className={styles.ContactUsWrapper}>
@@ -16,12 +18,27 @@ export const ContactUs = () => {
                 </div>
                 <div className={styles.ContactUsContactInfo}>
                     <h1><strong>КОНТАКТИ</strong></h1>
-                    <ul>
+                    {/* <ul>
                         <li>София 1618 <br /> ул. „Гусла No1 (кв. „Овча Купел”) <br /> Висше училище по застраховане и финанси <br /> ст. 405</li>
                         <li>00359 2 40 80 15</li>
                         <li><a href="mailto:pariikultura@vuzf.bg?">pariikultura@vuzf.bg</a></li>
+                    </ul> */}
 
-                    </ul>
+                    <table>
+                        <tr>
+                            <td>
+                            </td>
+                            <td>София 1618 ул. „Гусла No1 (кв. „Овча Купел”) Висше училище по застраховане и финанси ст. 405</td>
+                        </tr>
+                        <tr>
+                            <td>phone icon</td>
+                            <td>00359 2 40 80 15</td>
+                        </tr>
+                        <tr>
+                            <td>email icon</td>
+                            <td><a href="mailto:pariikultura@vuzf.bg?">pariikultura@vuzf.bg</a></td>
+                        </tr>
+                    </table>
                 </div>
 
             </div>
