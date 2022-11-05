bec58a53d519f0ce8a93272dd6dab8139fa57137
luskoeqk <77803201+luskoeqk@users.noreply.github.com>
2022-11-05 13:22:49 +0200

    Update ArchiveComponent.js

--- client/src/components/ArchiveComponent/ArchiveComponent.js
+++ client/src/components/ArchiveComponent/ArchiveComponent.js
@@ -71,34 +71,29 @@ export const ArchiveComponent = () => {
                 <div className={styles.ArchiveComponentCards}>
                     <iframe src="https://drive.google.com/file/d/12GIPAonrv6o6W3haDxZr-d2WI5GAu_92/preview" width="440" height="610" allow="autoplay"></iframe>
                     <h2>Lorem Ipsum</h2>
-                    <StyledH3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi rutrum sollicitudin orci, et gravida tellus pharetra vel. Etiam odio nisl, rhoncus ac diam quis, tristique sodales erat. Aliquam pellentesque tellus vitae egestas viverra. Pellentesque dictum, nibh in egestas suscipit, mi ante luctus ante, id aliquet mi nibh eget tortor. Suspendisse ac ante quis est tempus tristique. Sed auctor, sem eget placerat bibendum, erat lectus consectetur neque, a dignissim dolor nulla at turpis. Nam quis dignissim ex, in eleifend libero. Cras a quam lectus.</StyledH3>
                     <StyledH4>23.06.2022</StyledH4>
                 </div>
                 <div className={styles.ArchiveComponentCards}>
                     <iframe src="https://drive.google.com/file/d/1zvlZrUXtojV1I4si-Tu-TJSk5Q7jl8oC/preview" width="440" height="610" allow="autoplay"></iframe>
                     <h2>Згалавие</h2>
-                    <StyledH3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi rutrum sollicitudin orci, et gravida tellus pharetra vel. Etiam odio nisl, rhoncus ac diam quis, tristique sodales erat. Aliquam pellentesque tellus vitae egestas viverra. Pellentesque dictum, nibh in egestas suscipit, mi ante luctus ante, id aliquet mi nibh eget tortor. Suspendisse ac ante quis est tempus tristique. Sed auctor, sem eget placerat bibendum, erat lectus consectetur neque, a dignissim dolor nulla at turpis. Nam quis dignissim ex, in eleifend libero. Cras a quam lectus.</StyledH3>
                     <StyledH4>дата</StyledH4>
 
                 </div>
                 <div className={styles.ArchiveComponentCards}>
                     <iframe src="https://drive.google.com/file/d/1yF8rRXN4fzjz2JaTss--QxApDWcYXaWw/preview" width="440" height="610" allow="autoplay"></iframe>
                     <h2>Lorem Ipsum</h2>
-                    <StyledH3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi rutrum sollicitudin orci, et gravida tellus pharetra vel. Etiam odio nisl, rhoncus ac diam quis, tristique sodales erat. Aliquam pellentesque tellus vitae egestas viverra. Pellentesque dictum, nibh in egestas suscipit, mi ante luctus ante, id aliquet mi nibh eget tortor. Suspendisse ac ante quis est tempus tristique. Sed auctor, sem eget placerat bibendum, erat lectus consectetur neque, a dignissim dolor nulla at turpis. Nam quis dignissim ex, in eleifend libero. Cras a quam lectus.</StyledH3>
                     <StyledH4>дата</StyledH4>
 
                 </div>
                 <div className={styles.ArchiveComponentCards}>
                     <iframe src="https://drive.google.com/file/d/1dOy92YBg5lcBLBQXYy60EwHvmrpKaX6x/preview" width="440" height="610" allow="autoplay"></iframe>
                     <h2>Згалавие</h2>
-                    <StyledH3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi rutrum sollicitudin orci, et gravida tellus pharetra vel. Etiam odio nisl, rhoncus ac diam quis, tristique sodales erat. Aliquam pellentesque tellus vitae egestas viverra. Pellentesque dictum, nibh in egestas suscipit, mi ante luctus ante, id aliquet mi nibh eget tortor. Suspendisse ac ante quis est tempus tristique. Sed auctor, sem eget placerat bibendum, erat lectus consectetur neque, a dignissim dolor nulla at turpis. Nam quis dignissim ex, in eleifend libero. Cras a quam lectus.</StyledH3>
                     <StyledH4>дата</StyledH4>
 
                 </div>
                 <div className={styles.ArchiveComponentCards}>
                     <iframe src="https://drive.google.com/file/d/1WoHtVmZQEwU1zI8zTCV0BSADjicIZ18v/preview" width="440" height="610" allow="autoplay"></iframe>
                     <h2>Згалавие</h2>
-                    <StyledH3>Описание</StyledH3>
                     <StyledH4>дата</StyledH4>
 
                 </div>
